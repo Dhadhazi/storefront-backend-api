576c464bde354e54551ed3f4d6fc2823e10ae67e
Dhadhazi <dhadhazi@gmail.com>
2021-03-09 08:51:16 +0100

    utils tests

--- src/tests/utilsSpec.ts
+++ src/tests/utilsSpec.ts
@@ -1,12 +1,21 @@
 import { signToken, tokenToData } from "../utils/jwt";
+import { encryptPassword } from "../utils/password";
 
-describe("JWT test", () => {
+describe("JWT tests", () => {
   const token = signToken({ test: "test" });
   it("should return a string after getting payload", () => {
     expect(typeof token === "string").toBe(true);
   });
-  it("should return the the passed in object", () => {
+  it("should return the passed in object", () => {
     const data = tokenToData(token);
     expect(data.test).toEqual("test");
+  });
+});
+
+describe("Password tests", () => {
+  it("Should hash the password", async () => {
+    const password = await encryptPassword("password");
+    expect(password != "password").toBe(true);
+    expect(password.length).toBeGreaterThan(40);
   });
 });
