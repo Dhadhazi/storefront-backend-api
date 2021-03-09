144fc873d243b6a9d1003bb90894650eec377a05
Dhadhazi <dhadhazi@gmail.com>
2021-03-09 14:11:36 +0100

    Product tests added

--- src/tests/apiEndpointsSpec.ts
+++ src/tests/apiEndpointsSpec.ts
@@ -2,11 +2,14 @@ import supertest from "supertest";
 import { app } from "../server";
 import { signToken, tokenToData } from "../utils/jwt";
 import { User } from "../models/User";
+import { Product } from "../models/Product";
 
 const request = supertest(app);
 const BadUser = signToken({});
 const FakeUserForAuth = signToken({ user: {} });
 let RealUser: User;
+let product1: Product;
+let product2: Product;
 
 describe("API Endpoint Tests", () => {
   describe("/ endpoint test, server running", () => {
@@ -49,7 +52,7 @@ describe("API Endpoint Tests", () => {
         .send({ firstName: "Test", lastName: "Elek", password: "password" })
         .set("Authorization", `Bearer ${FakeUserForAuth}`);
       expect(response.status).toBe(200);
-      RealUser = tokenToData(response.text.slice(1, -1)).user;
+      RealUser = tokenToData(response.body).user;
       done();
     });
     it("List all users", async (done) => {
@@ -71,20 +74,55 @@ describe("API Endpoint Tests", () => {
       done();
     });
   });
+  describe("PRODUCTS/ endpoint", () => {
+    it("Create new product at /post", async (done) => {
+      const response = await request
+        .post("/products")
+        .type("form")
+        .send({ name: "APIProduct1", price: 2, category: "APITest" })
+        .set("Authorization", `Bearer ${FakeUserForAuth}`);
+      expect(response.status).toBe(200);
+      product1 = response.body;
+      expect(product1.category).toBe("APITest");
+      expect(product1.price).toBe(2);
+      expect(product1.name).toBe("APIProduct1");
+      done();
+    });
+    it("List all products", async (done) => {
+      const response = await request
+        .get("/products")
+        .expect("Content-Type", /json/);
+      expect(response.status).toBe(200);
+      expect(response.body.length).toBeGreaterThan(0);
+      done();
+    });
+    it("Get product by ID", async (done) => {
+      const response = await request
+        .get(`/products/${product1.id}`)
+        .expect("Content-Type", /json/);
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(product1);
+      done();
+    });
+    it("Get product by Category", async (done) => {
+      const response = await request
+        .get(`/products/category/${product1.category}`)
+        .expect("Content-Type", /json/);
+      expect(response.status).toBe(200);
+      expect(response.body[0]).toEqual(product1);
+      done();
+    });
+  });
 });
 
 /*
 #### Products
 - Index - GET products/
 - Show - GET products/:id
-- Create [token required] POST products/
+- Create [token required] POST products/ DONE
 - [OPTIONAL] Top 5 most popular products GET products/top/
 - [OPTIONAL] Products by category (args: product category) GET products/category/:category
 
-#### Users
-- Index [token required] GET users/
-- Show [token required] GET users/:id
-- Create [token required] POST users/
 
 #### Orders
 - Current Order by user (args: user id)[token required] orders/
