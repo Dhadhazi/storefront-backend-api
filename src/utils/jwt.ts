5df3b194f0a1c1586ab864cdebe857b19576a334
Dhadhazi <dhadhazi@gmail.com>
2021-07-13 14:55:19 +0200

    LAst any is purged

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,14 +1,17 @@
 import jwt from "jsonwebtoken";
 import config from "../config";
+import { User } from "../models/User";
+
+interface UserObject {
+  user: User;
+  test?: string; 
+}
 
 export function signToken(payload: string | object | Buffer): string {
   return jwt.sign(payload, config.SECRET);
 }
 
-/*
-Return is any type,  because it is universally extracts the token data, 
-therefore no shape should be configured so it can be used to any kind of token
-*/
-export function tokenToData(token: string): any {
-  return jwt.verify(token, config.SECRET);
+
+export function tokenToData(token: string) {
+  return jwt.verify(token, config.SECRET) as UserObject;
 }
